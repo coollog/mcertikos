11f70753725c1fe534f0bcae5d715e39d46bf7bb
Qingyang Chen <qingyang.chen@gmail.com>
2015-10-30 18:09:15 -0400

    added preempt sched

--- kern/trap/TTrapHandler/TTrapHandler.c
+++ kern/trap/TTrapHandler/TTrapHandler.c
@@ -11,6 +11,10 @@
 
 #include "import.h"
 
+// CUSTOM
+#include <lib/spinlock.h>
+static spinlock_t timer_lk;
+
 static void trap_dump(tf_t *tf)
 {
 	if (tf == NULL)
@@ -98,9 +102,11 @@ static int spurious_intr_handler (void)
 
 static int timer_intr_handler (void)
 {
+  spinlock_acquire(&timer_lk);
   // CUSTOM
   KERN_DEBUG("Got timer interrupt for cpu %d\n", get_pcpu_idx());
   sched_update();
+  spinlock_release(&timer_lk);
 
   intr_eoi();
   return 0;
